b8621d6e05b2f7198a5213a29c97e1e758a030d3
Sanket Jadhav <sanketvj.a01@gmail.com>
2022-10-18 11:51:38 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>App</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, ratione?
+      </p>
     </div>
   );
 }
